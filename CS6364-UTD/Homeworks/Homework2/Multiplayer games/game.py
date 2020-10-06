37a026811f46797b0bc7edd4b222c7cfe3bd6c0f
Pratik Deshpande <aristopratt.work@gmail.com>
2020-10-06 23:53:37 +0530

    starter code added.

--- CS6364-UTD/Homeworks/Homework2/Multiplayer games/game.py
+++ CS6364-UTD/Homeworks/Homework2/Multiplayer games/game.py
@@ -1,14 +1,34 @@
 
 INITIAL_STATE = [[1,None,None,2],[None,None,None,None],[None,None,None,None],[4,None,None,3]]
 
-def checkTerminalState(state):
-    if state[0][0] == 3:
-        return 3
-    elif state[0][3] == 4:
-        return 4 
-    elif state[3][3] == 1:
-        return 1
-    elif state[3][0] == 2:
-        return 2
-    else: return 
+class State(object):
+    currentPosition = ()
+    
+    def __init__(self,position):
+        self.currentPosition = position
+    
+    def generateValidPositions(position,action):
+        pass
 
+class Player(object):
+    state = None
+
+    def __init__(self,position):
+        self.state = State(position)
+        print("hi")
+    
+    def getAllActions():
+        pass
+
+class Game(object):
+    GameTree = {}
+    exploredStates = {}
+    Player1 = Player((1,1))
+    Player2 = Player((1,4))
+    Player3 = Player((4,4))
+    Player4 = Player((4,1))
+    
+    def generateGameTree(player,chance):
+
+        
+g = Game()
